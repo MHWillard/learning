13173bb92ba05da1c29033051057be317ab0fce6
MHWillard <mhwillard@gmail.com>
2024-07-26 16:00:12 -0400

    mainly working on Mongo/Azure integration

--- asp-net-mongo/WeaponStoreReact/obj/Debug/net8.0/WeaponStoreAPI.AssemblyInfo.cs
+++ asp-net-mongo/WeaponStoreReact/obj/Debug/net8.0/WeaponStoreAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeaponStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55082a832a1dda8a8dadd339f817c0ca9ac4b164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeaponStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeaponStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
