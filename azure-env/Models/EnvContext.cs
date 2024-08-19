5a9a670964cff2a3d95cd81d5c9e40142a74c955
MHWillard <mhwillard@gmail.com>
2024-08-19 15:39:20 -0400

    initial commit

---
+++ azure-env/Models/EnvContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace azure_env.Models
+{
+    public class EnvContext : DbContext
+    {
+        public EnvContext(DbContextOptions<EnvContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<EnvClass> EnvItems { get; set; } = null!;
+    }
+}
