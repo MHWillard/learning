4272777f247c9aff2353d112a5f86977775d274e
MHWillard <mhwillard@gmail.com>
2024-08-27 11:44:15 -0400

    turning minimal API into using EFCore

--- minimal-api/WarlordStore/Program.cs
+++ minimal-api/WarlordStore/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using WarlordStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+
+//Add the context to the database in order to use the in-memory database.
+builder.Services.AddDbContext<WeaponDb>(options => options.UseInMemoryDatabase("items"));
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "WarlordStore API",Description="Offering vital tools for all of your conquest needs.",Version="v1" });
@@ -20,5 +26,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "Hello World!");
+
+// Minimal API routes that access the Database class and uses the methods in there.
+
+//async operation: get the WeaponDb then away for it to return a list of weapons.
+app.MapGet("/weapons", async (WeaponDb db) => await db.Weapons.ToListAsync());
 
 app.Run();
